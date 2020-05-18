2270ab1b413ef571193dda831c382831932485f4
Mmeng Xuan <tr@xua.com>
2020-05-18 11:19:03 +0800

    //工具

--- app/src/main/java/com/bw/movie/util/NetWorkUtil.java
+++ app/src/main/java/com/bw/movie/util/NetWorkUtil.java
@@ -1,12 +1,17 @@
 package com.bw.movie.util;
 
-import com.bw.movie.Api;
-import com.bw.movie.ApiService;
+import com.bw.movie.url.Api;
+import com.bw.movie.apiservice.ApiService;
 
-import java.security.MessageDigest;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -18,7 +23,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * @MengXuanmengxuan
  * @日期2020/5/17
  * @项目名MengMoviebw
- * @类名NetWorkUtil
+ * @类名网络工具
  **/
 public class NetWorkUtil {
 
@@ -42,7 +47,7 @@ public class NetWorkUtil {
                 .addInterceptor(httpLoggingInterceptor)
                 .build();
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Api.URL)
+                .baseUrl(Api.URL_OUTER)
                 .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
@@ -52,5 +57,40 @@ public class NetWorkUtil {
 
     public ApiService getmApiService() {
         return mApiService;
+    }
+
+    public  void  getNoParameters(String url){
+        mApiService.getNoParameters(url)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe();
+
+    }
+    public  void  getHaveParameters(Class myclass,String url,Map<String , Object> map){
+        mApiService.getHaveParameters(url, map)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(ResponseBody responseBody) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
     }
 }
